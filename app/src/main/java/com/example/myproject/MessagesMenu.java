c7df8f2b2ddcabad69967e077ca3a6467fae7650
3288749273 <3288749273@qq.com>
2024-05-15 23:08:43 +0800

    资讯

--- app/src/main/java/com/example/myproject/MessagesMenu.java
+++ app/src/main/java/com/example/myproject/MessagesMenu.java
@@ -1,4 +1,24 @@
 package com.example.myproject;
 
-public class MessagesMenu {
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MessagesMenu extends AppCompatActivity {
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_messages); // 确保布局文件名称正确
+
+        mWebView = findViewById(R.id.webView);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient()); // 确保网页在WebView中加载，而不是在浏览器中打开
+        mWebView.loadUrl("https://info.swufe.edu.cn/xwgg/zytz.htm");
+    }
 }
