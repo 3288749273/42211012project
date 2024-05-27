036fa2241cf57e698ed6de67473e004129a5c3d0
3288749273 <3288749273@qq.com>
2024-05-27 22:27:00 +0800

    常见问题（未设置FAQ）

--- app/src/main/java/com/example/myproject/HomeMenu.java
+++ app/src/main/java/com/example/myproject/HomeMenu.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -15,10 +16,45 @@ public class HomeMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_home);
+        //上导航
+        LinearLayout homequestions = findViewById(R.id.homequsetions);
+        LinearLayout homebook = findViewById(R.id.homebook);
+        LinearLayout homeduty = findViewById(R.id.homeduty);
+        LinearLayout homemore = findViewById(R.id.homemore);
+        //下导航
         RelativeLayout menu01 = findViewById(R.id.menu01);
         RelativeLayout menu02 = findViewById(R.id.menu02);
         RelativeLayout menu03 = findViewById(R.id.menu03);
         RelativeLayout menu04 = findViewById(R.id.menu04);
+
+        homequestions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeMenu.this, QuestionsHome.class);
+                startActivity(intent);
+            }
+        });
+        homebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeMenu.this, HomeMenu.class);
+                startActivity(intent);
+            }
+        });
+        homeduty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeMenu.this, HomeMenu.class);
+                startActivity(intent);
+            }
+        });
+        homemore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeMenu.this, HomeMenu.class);
+                startActivity(intent);
+            }
+        });
 
         menu01.setOnClickListener(new View.OnClickListener() {
             @Override
