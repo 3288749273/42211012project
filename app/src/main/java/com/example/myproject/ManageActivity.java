dddaab49450a5a2b47f0cb9866cf4dc618f24f2a
3288749273 <3288749273@qq.com>
2024-05-29 20:45:04 +0800

    管理员

--- app/src/main/java/com/example/myproject/ManageActivity.java
+++ app/src/main/java/com/example/myproject/ManageActivity.java
@@ -1,6 +1,7 @@
 package com.example.myproject;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,10 +31,18 @@ public class ManageActivity extends AppCompatActivity {
             if (db.checkAdminUser(adminID, adminPassword)) {
                 Toast.makeText(ManageActivity.this, "管理员登录成功", Toast.LENGTH_SHORT).show();
                 showManageScreen();
+                saveLoginState(adminID);
             } else {
                 Toast.makeText(ManageActivity.this, "登录失败，请检查账号和密码", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+    private void saveLoginState(String adminID) {
+        SharedPreferences sharedPreferences = getSharedPreferences("user_prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("admin_user_id", adminID); // 保存用户ID
+        editor.putBoolean("admin_is_logged_in", true); // 保存登录状态
+        editor.apply();
     }
 
     private void showManageScreen() {
